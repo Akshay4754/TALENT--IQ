ee4155fc971c7de3d9f267dacd0d5ba3bd42b31d
Akshay Anand <akshayanand630@gmail.com>
2026-02-19 17:01:07 +0530

    changes to be committed:

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,6 @@ app.use(cors({ origin: ENV.CLIENT_URL ,credentials: true  }));
 
 app.use("/api/inngest", serve({ client: inngest, functions }) );
 
-
 app.get("/health", (req, res) => {
 res.status(200).json({ msg: "api is up and running" });
 }); 
