ce44c682c9317884670a41e53748ae79fdb8dbf7
Akshay Anand <akshayanand630@gmail.com>
2026-02-19 17:08:37 +0530

    fixing db

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,25 +1,24 @@
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-name: {
-type: String,
-required: true,
-},
-email: { 
-type: String,
-required: true,
-unique: true,
-},
-profileImage : {
-type: String,
-default: "",
-}, 
-clerkId:{
-type: String,
-required: true,
-unique: true,
-},
-timestamps: true // createdAt, updatedAt
-});
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    profileImage: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
